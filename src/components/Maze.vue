6162095978d887781e78cc2ed329c8d917d91eba
DomJob <dominic.jobin@gmail.com>
2019-11-23 11:40:01 -0500

    yay movement

--- src/components/Maze.vue
+++ src/components/Maze.vue
@@ -2,12 +2,12 @@
   <div syle="width:100%;">
     <table style="margin: auto;" cellspacing="0">
       <thead>
-        <tr v-for="(row, headerKey) in maze.data" :key="headerKey">
+        <tr v-for="(row, i) in maze.data" :key="i">
           <td
             width="10"
             height="10"
-            v-for="(cell, headerKey2) in row"
-            :key="headerKey2"
+            v-for="(cell, j) in row"
+            :key="j"
             :style="{
               borderRight: cell.right == 0 ? '1px solid black' : '',
               borderLeft: cell.left == 0 ? '1px solid black' : '',
@@ -15,7 +15,7 @@
               borderBottom: cell.bottom == 0 ? '1px solid black' : ''
             }"
           >
-            {{ '\xa0' }}
+            {{ j == posx && i == posy ? 'X' : '\xa0' }}
           </td>
         </tr>
       </thead>
@@ -26,18 +26,44 @@
 <script>
 import labyrinthApi from '../api/labyrinthApi';
 
-export default {
+var app = {
   name: 'Maze',
   props: {
     level: String
   },
   data: () => ({
-    maze: null
+    posx: 0,
+    posy: 0,
+    maze: { data: [] }
   }),
   async mounted() {
     this.maze = await labyrinthApi.getMaze('advanced');
+
+    window.addEventListener('keydown', e => {
+      this.handleKey(e.code);
+    });
+  },
+  methods: {
+    handleKey(e) {
+      console.log(this);
+      switch (e) {
+        case 'ArrowUp':
+          if (this.maze.data[this.posy][this.posx].top) this.posy--;
+          break;
+        case 'ArrowDown':
+          if (this.maze.data[this.posy][this.posx].bottom) this.posy++;
+          break;
+        case 'ArrowRight':
+          if (this.maze.data[this.posy][this.posx].right) this.posx++;
+          break;
+        case 'ArrowLeft':
+          if (this.maze.data[this.posy][this.posx].left) this.posx--;
+          break;
+      }
+    }
   }
 };
+export default app;
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
